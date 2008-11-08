f005d17030e35cb0e9eed80e8e59d1d653fd30f1
Alper Kanat <tunix@raptiye.org>
2008-11-08 22:46:08 +0000

    * gezegen/index.html.tmpl, www/common.js: fixed a bug in the JS code.. thanks to doruk fisek.. * added planet rules link that's in gezegen crew's blog..

--- www/common.js
+++ www/common.js
@@ -1,15 +1,13 @@
 var entries = []
 
 function toggleEntry(id) {
-	// hide summary
-	$("#summary_" + id).slideToggle()
 	if ($("#summary_" + id).css("display") == "none") {
 		// change the link text
-		$("#toggle_content_" + id).text("Bu yazıyla ilgili detayları okumak için tıklayın.")
+		$("#toggle_content_" + id).text("Bu yazının devamını okumak için tıklayın.")
 	} else {
 		// change the link text
-		$("#toggle_content_" + id).text("Bu yazının detaylarını kapatmak için tıklayın.")
+		$("#toggle_content_" + id).text("Bu yazının devamını kapatmak için tıklayın.")
 	}
-	// show the content
-	$("#content_" + id).slideToggle();
+	$("#content_" + id).slideToggle()
+	$("#summary_" + id).slideToggle()
 }
